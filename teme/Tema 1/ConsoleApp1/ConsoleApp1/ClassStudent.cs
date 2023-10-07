3594ff7354f63e993464962dc5866ac9cb2e184b
Dogaru Mihail Danut <106425405+Matoka26@users.noreply.github.com>
2023-10-07 16:26:37 +0300

    Add files via upload

---
+++ teme/Tema 1/ConsoleApp1/ConsoleApp1/ClassStudent.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema01
+{
+    internal class Student
+    {
+        public string nume { get; set; }
+        public string prenume { get; set; }
+        public int? codStudent { get; set; }
+        public char initialaTata { get; set; }
+        public List<Materie>? materii { get; set; }
+
+        public Student()
+        {
+            nume = "necunoscut";
+            prenume = "necunoscut";
+            initialaTata = '?';
+            codStudent = null;
+            materii = new List<Materie>();
+        }
+
+        public void afisareNume()
+        {
+            Console.WriteLine(codStudent + ": " + nume + " " + initialaTata + " " + prenume);
+        }
+        public void afisareMaterie()
+        {
+            if (materii.Count != 0)
+            {
+                Console.Write("Materii:");
+                foreach (Materie mat in materii)
+                {
+                    Console.Write(mat.numeMaterie + " ");
+                }
+            }
+            else { Console.WriteLine("Nu e inscris la nimic"); }
+            Console.Write("\n\n");
+        }
+    } 
+}
+
---
+++ teme/Tema 1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tema01;
+
+
+var listaStudenti = new List<Student>();
+
+listaStudenti.Add(new Student());
+listaStudenti[0].nume = "Popescu";
+listaStudenti[0].prenume = "Ion";
+listaStudenti[0].initialaTata = 'S';
+listaStudenti[0].codStudent = 6464;
+listaStudenti[0].materii.Add(new Materie("Mate",10,"2h",7));
+listaStudenti[0].materii.Add(new Materie("Sport", 6, "4h", 1));
+
+
+listaStudenti.Add(new Student());
+listaStudenti[1].nume = "Ionescu";
+listaStudenti[1].prenume = "Andrei";
+listaStudenti[1].initialaTata = 'R';
+listaStudenti[1].codStudent = 5000;
+listaStudenti[1].materii.Add(new Materie());
+
+
+listaStudenti.Add(new Student());
+listaStudenti[2].nume = "Niculescu";
+listaStudenti[2].prenume = "Alexandru";
+listaStudenti[2].initialaTata = 'S';
+listaStudenti[2].codStudent = 5452;
+
+
+
+foreach (Student st in listaStudenti)
+{
+    st.afisareNume();
+    st.afisareMaterie();
+}
