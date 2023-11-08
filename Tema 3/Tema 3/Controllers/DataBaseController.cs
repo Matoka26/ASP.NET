6ba6fddeb9de9847763c897e39dd192251955307
Dogaru Mihail Danut <106425405+Matoka26@users.noreply.github.com>
2023-11-08 20:46:03 +0200

    Add files via upload

---
+++ Tema 3/Tema 3/Controllers/DataBaseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Tema3.Data;
+
+namespace Tema3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DatabaseController : ControllerBase
+    {
+        private readonly AppDbContext _dbContext;
+
+        public DatabaseController(AppDbContext dbcontext)
+        {
+            _dbContext = dbcontext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _dbContext.Movies.ToListAsync());
+        }
+    }
+}
---
+++ Tema 3/Tema 3/Models/Base/IBaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Tema3.Models.Base
+{
+    public class IBaseEntity
+    {
+        Guid Id { get; set; }
+        DateTime? DateTime { get; set; }
+        DateTime? LastModified { get; set; }   
+
+    }
+}
